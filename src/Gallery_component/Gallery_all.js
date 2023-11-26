76404db890732aff16db29810311d49e0c8995c1
booo <wcd065864@gmail.com>
2023-11-26 22:54:17 +0900

    modify: Gallery_all(11/26)

--- src/Gallery_component/Gallery_all.js
+++ src/Gallery_component/Gallery_all.js
@@ -1,18 +1,8 @@
 import { Link } from "react-router-dom";
 import "../css/Gallery.css";
-import all_sort from "../img/all_sort.png";
-import next from "../img/next.png";
-import prev from "../img/prev.png";
-import first from "../img/first.jpg";
-import second from "../img/second.jpg";
-import third from "../img/third.jpg";
-import fourth from "../img/fourth.jpg";
-import fifth from "../img/fifth.jpg";
 import React, { useState } from "react";
-import { Swiper, SwiperSlide } from "swiper/react";
 import "swiper/css";
 import "swiper/css/effect-cube";
-import { EffectCube, Navigation } from "swiper/modules";
 import { motion } from "framer-motion";
 
 const All = () => {
