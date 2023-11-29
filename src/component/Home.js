657d417bdd9069beab7a67e5611c1b05a0527eda
booo <wcd065864@gmail.com>
2023-11-29 21:15:20 +0900

    modify: home

--- src/component/Home.js
+++ src/component/Home.js
@@ -4,6 +4,17 @@ import fifth from "../img/fifth.jpg";
 
 
 function HOME() {
+    const Text = ({ value }) => (
+      <div className="text_home">
+        {value.split("").map((char, i) => (
+          <div className="letter" style={{ "--delay": `${i * 0.2}s` }}>
+            <span className="source">{char}</span>
+            <span className="shadow">{char}</span>
+            <span className="overlay">{char}</span>
+          </div>
+        ))}
+      </div>
+    );
   return (
     <motion.div
       className="body"
@@ -34,31 +45,43 @@ function HOME() {
           <div className="box1_par">
             <div className="box_1"></div>
             <div className="box_1_text">
-              <h2>GraphiteArt</h2>
-              <h2>Museum</h2>
+              <Text value="GraphiteArt" />
+              <Text value="Museum" />
             </div>
           </div>
           <div className="box2_par">
             <div className="box_2">
               <div className="box_flip_inner">
-                <div className="box_flip_front_1"></div>
-                <div className="box_flip_back_1"></div>
+                <div className="box_flip_front_1">
+                  <h2>테스트</h2>
+                </div>
+                <div className="box_flip_back_1">
+                  <h2>테스트</h2>
+                </div>
               </div>
             </div>
           </div>
           <div className="box3_par">
             <div className="box_3">
               <div className="box_flip_inner">
-                <div className="box_flip_front_2"></div>
-                <div className="box_flip_back_2"></div>
+                <div className="box_flip_front_2">
+                  <h2>테스트</h2>
+                </div>
+                <div className="box_flip_back_2">
+                  <h2>테스트</h2>
+                </div>
               </div>
             </div>
           </div>
           <div className="box4_par">
             <div className="box_4">
               <div className="box_flip_inner">
-                <div className="box_flip_front_3"></div>
-                <div className="box_flip_back_3"></div>
+                <div className="box_flip_front_3">
+                  <h2>테스트</h2>
+                </div>
+                <div className="box_flip_back_3">
+                  <h2>테스트</h2>
+                </div>
               </div>
             </div>
           </div>
