8175b86841a3e12fff9c93e7a9351a81f2bf2b32
booo <wcd065864@gmail.com>
2023-11-29 23:31:42 +0900

    Completion : homeComponent

--- src/component/Home.js
+++ src/component/Home.js
@@ -52,11 +52,14 @@ function HOME() {
           <div className="box2_par">
             <div className="box_2">
               <div className="box_flip_inner">
-                <div className="box_flip_front_1">
-                  <h2>테스트</h2>
+                <div className="box_flip_front_1"></div>
+                <div className="box_flip_front_1_text">
+                  <h2>Value</h2>
                 </div>
                 <div className="box_flip_back_1">
-                  <h2>테스트</h2>
+                  <h2 className="text">
+                    We will provide the best artistic value.
+                  </h2>
                 </div>
               </div>
             </div>
@@ -64,11 +67,12 @@ function HOME() {
           <div className="box3_par">
             <div className="box_3">
               <div className="box_flip_inner">
-                <div className="box_flip_front_2">
-                  <h2>테스트</h2>
+                <div className="box_flip_front_2"></div>
+                <div className="box_flip_front_2_text">
+                  <h2>Communication</h2>
                 </div>
                 <div className="box_flip_back_2">
-                  <h2>테스트</h2>
+                <h2>We will give you the feeling of communicating with art.</h2>
                 </div>
               </div>
             </div>
@@ -76,11 +80,14 @@ function HOME() {
           <div className="box4_par">
             <div className="box_4">
               <div className="box_flip_inner">
-                <div className="box_flip_front_3">
-                  <h2>테스트</h2>
+                <div className="box_flip_front_3"></div>
+                <div className="box_flip_front_3_text">
+                  <h2>Experience</h2>
                 </div>
                 <div className="box_flip_back_3">
-                  <h2>테스트</h2>
+                  <h2 className="text">
+                    We will give you the best experience.
+                  </h2>
                 </div>
               </div>
             </div>
