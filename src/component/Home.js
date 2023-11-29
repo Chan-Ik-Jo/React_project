6e3acb58a15ff25061fdfdf7fd3520d749a1e113
booo <wcd065864@gmail.com>
2023-11-29 12:51:12 +0900

    modify: homeComponent

--- src/component/Home.js
+++ src/component/Home.js
@@ -5,12 +5,14 @@ import fifth from "../img/fifth.jpg";
 
 function HOME() {
   return (
-    <motion.div className="body" 
-                initial={{opacity:0}}
-                animate={{opacity:1}}
-                exit={{opacity:0}}>
+    <motion.div
+      className="body"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      exit={{ opacity: 0 }}
+    >
       <div className="container">
-        <div className="imgSection">
+        {/* <div className="imgSection">
           <img className="bannerImage" src={fifth} alt="" />
         </div>
         <div className="imgText">
@@ -19,6 +21,39 @@ function HOME() {
           <span className="subLine">The flower who has made</span>
           <br />
           <span className="artistName">Artist : unknown</span>
+        </div> */}
+        <div className="p">
+          <svg
+            class="stroke-animation"
+            // viewBox="0 0 1000 1000"
+            style={{ backgroundColor: "white" }}
+          >
+            <path
+              className="per"
+              stroke="black"
+              stroke-width="7"
+              fill="transparent"
+              d="M 800 0 V 0 795"
+            />
+            <path
+              className="horizon"
+              stroke="black"
+              stroke-width="7"
+              fill="transparent"
+              d="M 5 600 H 5 1050"
+            />
+          </svg>
+        </div>
+        <div className="c">
+          <div className="box_1">
+          </div>
+          <div className="box_2">
+          </div>
+          <div className="box_3">
+          </div>
+          <div className="box_4">
+          </div>
+
         </div>
       </div>
     </motion.div>
